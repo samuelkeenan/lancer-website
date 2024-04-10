53677fd034ba50a6493365e8ef019a909681185a
samuelkeenan <keenan.samuel@gmail.com>
2024-04-10 19:43:08 -0300

    Update Debrief.vue

--- src/Debrief.vue
+++ src/Debrief.vue
@@ -1,16 +1,125 @@
 <template>
-  <div>
-    <h1>About Us</h1>
-    <p>This is the about page content.</p>
-  </div>
+  <nav>
+    <router-link to="/debrief">Debrief</router-link>  
+  </nav>
+  <Header :header="this.header" />
+  <svg
+    style="visibility: hidden; position: absolute;"
+    width="0"
+    height="0"
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+  >
+    <defs>
+      <filter id="round">
+        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+        <feColorMatrix
+          in="blur"
+          mode="matrix"
+          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
+          result="goo"
+        />
+        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+      </filter>
+    </defs>
+  </svg>
+  <audio autoplay>
+    <source src="/startup.ogg" type="audio/ogg" />
+  </audio>
+  <Footer/>
 </template>
 
 <script>
+import Header from './components/layout/Header.vue';
+import Footer from './components/layout/Footer.vue';
+import Mission from './components/Mission.vue';
+import Pilot from './components/Pilot.vue';
+import Markdown from 'vue3-markdown-it';
+
 export default {
-  // Component logic goes here
+  components: {
+    Header,
+    Footer,
+    Mission,
+    Pilot,
+    Markdown
+  },
+
+  data() {
+    return {
+
+      "header": {
+        "planet": "Ishtar",
+        "year": "5016u",
+        "system": "Anu-ki",
+        "gate": "Karakoram-Saltoro-Kangri",
+        "ring": "Karakoram-Line",
+        "headerTitle": "Union",
+        "headerSubtitle": "Naval Department",
+        "subheaderTitle": "Lancer Cadets",
+        "subheaderSubtitle": "Delta-Squad",
+      },
+      "options":{
+        "eventsMarkdownPerMission": true
+      }
+    }
+  },
+
+  created() {
+    this.loadMissionMarkdown()
+    this.loadEventsMarkdown()
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    selectMission(mission) {
+      this.mission_slug = mission.slug;
+      this.loadMissionMarkdown()
+      if(this.options.eventsMarkdownPerMission){
+        this.loadEventsMarkdown();
+      }
+    },
+    loadMissionMarkdown() {
+      let self = this;
+      let md = `/missions/${self.mission_slug}.md`
+      var client = new XMLHttpRequest();
+      client.open('GET', md);
+      client.onreadystatechange = function () {
+        self.current_md = client.responseText;
+      }
+      client.send();
+    },
+    loadEventsMarkdown() {
+      let self = this;
+      let md = "";
+
+      if(self.options.eventsMarkdownPerMission){
+        md = `/events/${self.mission_slug}.md`
+      }
+      else {
+        md = "/events.md"
+      }
+
+      var client = new XMLHttpRequest();
+      client.open('GET', md);
+      client.onreadystatechange = function () {
+        self.events = client.responseText;
+      }
+      client.send();
+    }
+  }
+
 }
 </script>
 
-<style scoped>
-/* Component-specific styles go here */
+
+<style lang="scss">
+#app {
+  width: 1902px;
+  height: 910px;
+  overflow: hidden;
+}
 </style>
