43a26434a713bd00acb44af9eeef621fb08d5722
samuelkeenan <keenan.samuel@gmail.com>
2024-04-10 19:12:38 -0300

    Create debrief.vue

---
+++ debrief.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About Us</h1>
+    <p>This is the about page content.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  // Component logic goes here
+}
+</script>
+
+<style scoped>
+/* Component-specific styles go here */
+</style>
