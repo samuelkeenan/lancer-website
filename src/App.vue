0a321c59f282879279ec1137e2ffcf5e63599bc9
samuelkeenan <keenan.samuel@gmail.com>
2024-04-10 16:45:55 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -95,13 +95,13 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Bug-Hunt",
+          "name": "Defend-the-Embassy",
           "status": "start"
         },
       ],
       "pilots": [
         {
-          "callsign": "Jinzo",
+          "callsign": "Bluebird",
           "alias": "Shayan Arsenal",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
           "corpro": "GMS",
@@ -109,7 +109,7 @@ export default {
           "mech": "Type - Worst Beat"
         },
         {
-          "callsign": "Kal",
+          "callsign": "Decaf",
           "alias": "Vier Flau",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
@@ -117,7 +117,7 @@ export default {
           "mech": "Necrophile"
         },
         {
-          "callsign": "Starboard",
+          "callsign": "Lancelot",
           "alias": "Maria",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
@@ -125,7 +125,7 @@ export default {
           "mech": "Scarlet Judicator"
         },
         {
-          "callsign": "Badlands",
+          "callsign": "Starling",
           "alias": "Marlene Gatsby",
           "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
           "corpro": "GMS",
@@ -133,7 +133,7 @@ export default {
           "mech": "Mayfly"
         },
         {
-          "callsign": "Compadre",
+          "callsign": "Wing",
           "alias": 'Clint "CC" Cruz',
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
@@ -142,14 +142,14 @@ export default {
         },
       ],
       "header": {
-        "planet": "Hercynia",
-        "year": "5014u",
-        "system": "Ardennes-3",
-        "gate": "Atlas-Quanokrim",
-        "ring": "Atlas-Line",
-        "headerTitle": "Mirrorsmoke",
-        "headerSubtitle": "Mercenary Company",
-        "subheaderTitle": "Crisis Response",
+        "planet": "Ishtar",
+        "year": "5016u",
+        "system": "Anu-Ki",
+        "gate": "Karakoram-Saltoro-Kangri",
+        "ring": "Karakoram-Line",
+        "headerTitle": "Union",
+        "headerSubtitle": "Naval Department",
+        "subheaderTitle": "Lancer Trainees",
         "subheaderSubtitle": "Delta-Echo-Echo-Zulu",
       },
       "options":{
