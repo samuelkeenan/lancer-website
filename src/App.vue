ab5676dd4eba8f9150ece1276bea27e103beb790
samuelkeenan <keenan.samuel@gmail.com>
2024-04-10 18:51:06 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,7 @@ export default {
       "missions": [
         {
           "slug": "001",
-          "name": "Hold-the-Embassy",
+          "name": "Defend-the-Embassy",
           "status": "start"
         },
       ],
