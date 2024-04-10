efe132cc92daa101e08b279c5cf6b4d39e3a0642
samuelkeenan <keenan.samuel@gmail.com>
2024-04-10 19:56:29 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
       <router-link to="/">App</router-link>
-    <router-link to="/">Debrief</router-link>  
+    <router-link to="/debrief">Debrief</router-link>  
   </nav>
   <Header :header="this.header" />
   <div class="content-container">
