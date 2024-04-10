80741c77348c22c9f779b4eafc2cc0e9887b7d67
samuelkeenan <keenan.samuel@gmail.com>
2024-04-10 19:17:54 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import Oruga from '@oruga-ui/oruga-next'
 import '@oruga-ui/oruga-next/dist/oruga.min.css'
-
-
 import './assets/styles/main.scss'
 
+// Import Vue Router
+import { createRouter, createWebHistory } from 'vue-router'
+
+// Import your components for routing
+import Debrief from './views/Debrief.vue'
+
+// Create router instance
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/debrief',
+      name: 'Debrief',
+      component: Debrief
+    },
+    // You can add more routes here
+  ]
+})
+
 createApp(App)
-	.use(Oruga)
-	.mount('#app')
+  .use(Oruga)
+  .use(router) // Use the router instance
+  .mount('#app')
