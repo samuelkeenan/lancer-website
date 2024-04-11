08d6429c3c87cb900860f961be0b031ef453ee1c
samuelkeenan <keenan.samuel@gmail.com>
2024-04-11 19:08:28 -0300

    Update Header.vue

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,13 @@
 <template>
   <header class="clipped-x-large-forward">
     <div class="title clipped-x-large-forward">
-    <img class="logo" src="/icons/company-icon.svg" />
+    <div class="dropdown">
+      <img class="logo" src="/icons/company-icon.svg" />
+      <div class="dropdown-content">
+        <router-link to="/">App</router-link>
+        <router-link to="/debrief">Debrief</router-link>  
+      </div>
+    </div>
       <div class="title-container">
         <div class="title-row" id="title-first-line">
           <span id="title-header">{{ header.headerTitle }}</span>
